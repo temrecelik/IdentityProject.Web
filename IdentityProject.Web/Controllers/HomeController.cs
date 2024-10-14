2e8d80d94859052ffa3327883f196fd719c30eb3
temrecelik <temrecelik21@gmail.com>
2024-10-14 13:15:10 +0300

    SignIn Razor Page Created

--- IdentityProject.Web/Controllers/HomeController.cs
+++ IdentityProject.Web/Controllers/HomeController.cs
@@ -29,7 +29,13 @@ namespace IdentityProject.Web.Controllers
             return View();
         }
 
-       
+        public IActionResult SignIn()
+        {
+            return View();
+
+        }
+
+
         public IActionResult SignUp()
         {
             return View();
---
+++ IdentityProject.Web/Models/ViewModels/SignInViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityProject.Web.Models.ViewModels
+{
+    public class SignInViewModel
+    {
+        public SignInViewModel(){ } //boş constructur Identity için gerekli
+
+       
+        
+
+        public SignInViewModel(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        [EmailAddress(ErrorMessage = "Mail formatı email@example.com formatında olmalıdır.")]
+        [Required(ErrorMessage = "Mail Alanı Boş Bırakılamaz.")]
+        [Display(Name = "Mail Adresi : ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Şifre Boş Bırakılamaz.")]
+        [Display(Name = "Şifre : ")]
+        public string Password { get; set; }
+
+
+        public bool RememberMe { get; set; }    
+
+    }
+}
