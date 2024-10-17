37e7b219733a6e8262d61d87f3ca4fa2f862e5ff
temrecelik <temrecelik21@gmail.com>
2024-10-17 13:24:55 +0300

    Codef of Member Page's Method have been written

--- IdentityProject.Web/Controllers/MemberController.cs
+++ IdentityProject.Web/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityProject.Web.Models.Entities;
+using IdentityProject.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,22 +10,38 @@ namespace IdentityProject.Web.Controllers
 	public class MemberController : Controller
 	{
 		private readonly SignInManager<User> _signInManager;
+		private readonly UserManager<User> _userManager;
 
-		public MemberController(SignInManager<User> signInManager)
-		{
-			_signInManager = signInManager;
-		}
+        public MemberController(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
 
-		/*Çıkış yapma işlemi Authorize olmalıdır çünkü giriş yapmadan çıkış yapılmaz*/
+        /*Çıkış yapma işlemi Authorize olmalıdır çünkü giriş yapmadan çıkış yapılmaz Identity'den gelen özellik*/
 
-		public async Task /*<IActionResult>*/ LogOut()
+        public async Task /*<IActionResult>*/ LogOut()
 		{
 			await _signInManager.SignOutAsync();
 			//return RedirectToAction("Index","Home");
 		}
 
-		public IActionResult Index()
+		public  async Task<IActionResult> Index()
 		{
+
+			var currentUser = await _userManager.FindByNameAsync(User.Identity!.Name!);
+			if (currentUser != null)
+			{
+				UserInformationViewModel userInformationViewModel = new()
+				{
+					UserName = currentUser.UserName!,
+					Email = currentUser.Email!,
+					phoneNumber = currentUser.PhoneNumber,
+				};
+				return View(userInformationViewModel);
+			}
+			else
+				ModelState.AddModelError(string.Empty, "Kullanıcı bilgileri bulunamadı.");
 			return View();
 		}
 	}
---
+++ IdentityProject.Web/Models/ViewModels/UserInformationViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityProject.Web.Models.ViewModels
+{
+    public class UserInformationViewModel
+    {
+        public string? UserName { get; set; } = null!; //null olamaz
+        public string? Email { get; set; }
+        public string? phoneNumber { get; set; } //? ile nullable olabildiği anlamına gelir
+
+    }
+}
